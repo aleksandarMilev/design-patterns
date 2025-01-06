333176c5b3ddfd34da718ea96f1cd2f109653a31
aleksandarMilev <aleksandarmilev23@gmail.com>
2025-01-06 10:06:12 +0200

    Singleton

---
+++ DesignPatterns.Creational/Singleton/Singleton.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class Singleton<T> 
+        where T : class, new()
+    {
+        private readonly static T value = new();
+
+        private Singleton() { }
+
+        public static T GetInstance() => value;
+    }
+}
--- DesignPatterns.StartUp/Program.cs
+++ DesignPatterns.StartUp/Program.cs
@@ -1,7 +1,27 @@
 ﻿namespace DesignPatterns.StartUp
 {
+    using Creational.Singleton;
+
     internal class Program
     {
-        private static void Main() => Console.WriteLine("Hello, World!");
+        private static void Main()
+        {
+            //Creational
+            //1
+            var mySingletonClass = GetSingleton<MyTestClass>();
+        }
+
+        //Creational
+        //1
+        private static T GetSingleton<T>() 
+            where T : class, new() 
+                => Singleton<T>.GetInstance();
+    }
+
+    internal class MyTestClass
+    {
+        public string? MyProp { get; set; }
+
+        public string? MyMethod() => this.MyProp;
     }
 }
